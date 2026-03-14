576443615345b41c7b746499dc3c1dcfb3c23a0d
Chheng Sombathoudom <sombathoudom.chheng@cogetel.com.kh>
2026-03-14 08:54:18 +0700

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@next-js-and-electron/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
---
+++ apps/web/src/utils/orpc.ts
@@ -0,0 +1,27 @@
+import type { AppRouterClient } from "@next-js-and-electron/api/routers/index";
+import { createORPCClient } from "@orpc/client";
+import { RPCLink } from "@orpc/client/fetch";
+import { createTanstackQueryUtils } from "@orpc/tanstack-query";
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      toast.error(`Error: ${error.message}`, {
+        action: {
+          label: "retry",
+          onClick: query.invalidate,
+        },
+      });
+    },
+  }),
+});
+
+export const link = new RPCLink({
+  url: `${typeof window !== "undefined" ? window.location.origin : "http://localhost:3001"}/api/rpc`,
+});
+
+export const client: AppRouterClient = createORPCClient(link);
+
+export const orpc = createTanstackQueryUtils(client);
